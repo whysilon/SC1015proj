ab7ca7823d799d609d6acd4e354e259965d858a6
Han <77002201+harpervin@users.noreply.github.com>
2022-03-28 15:59:03 +0800

    collected user followers + user total view count

--- twitch_api.py
+++ twitch_api.py
@@ -3,6 +3,8 @@ from twitchAPI.oauth import UserAuthenticator
 from twitchAPI.types import AuthScope
 import pandas as pd
 from csv import DictWriter
+import csv
+import os.path
 
 app_id = 'oatab7pvysht8ho60uwl3qft7iapy0'
 app_secret = 'gh70o6bplxah7aiiq8xoyof7woii59'   
@@ -16,14 +18,27 @@ token, refresh_token = auth.authenticate()
 twitch.set_user_authentication(token, target_scope, refresh_token)
 
 stream_info = twitch.get_streams(first=100)
-print(stream_info)
 
-headers = ['id', 'user_id', 'user_login', 'user_name', 'game_id', 'game_name', 'type', 'title', 'viewer_count', 'started_at', 'language', 'thumbnail_url', 'tag_ids', 'is_mature']
+#print(stream_info)
+
+filename = 'twitch_data.csv'
+headers = ['id', 'user_id', 'user_login', 'user_name', 'game_id', 'game_name', 'type', 'title', 'viewer_count', 'started_at', 'language', 'thumbnail_url', 'tag_ids', 'is_mature', 'follow_count','total_views']
 
 data = stream_info['data']
 
-with open('twitch_data.csv', 'w', newline='') as f:
+fileEmpty = os.stat(filename).st_size == 0
+
+with open(filename, 'a', newline='') as f:
     d = DictWriter(f, fieldnames=headers)
-    d.writeheader()
+    if fileEmpty:
+        d.writeheader()
+    #print(type(data))
+    for row in data:
+        #print(row['user_id'])
+        follow_count = twitch.get_users_follows(to_id=row['user_id'])
+        view_count = twitch.get_users(logins=row['user_login'])
+        row['total_views'] = view_count['data'][0]['view_count']
+        row['follow_count'] = follow_count['total']
+    print('done')
     d.writerows(data)
     f.close()
