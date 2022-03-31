09b4f35b41346b5fa3a73a9a67a923d30736aa69
whysilon <80097678+whysilon@users.noreply.github.com>
2022-03-31 20:44:49 +0800

    New API algo

--- twitch_api.py
+++ twitch_api.py
@@ -4,7 +4,9 @@ from twitchAPI.types import AuthScope
 import pandas as pd
 from csv import DictWriter
 import csv
-import os.path
+import os
+import random
+
 
 app_id = 'oatab7pvysht8ho60uwl3qft7iapy0'
 app_secret = 'gh70o6bplxah7aiiq8xoyof7woii59'   
@@ -17,28 +19,56 @@ token, refresh_token = auth.authenticate()
 # add User authentication
 twitch.set_user_authentication(token, target_scope, refresh_token)
 
-stream_info = twitch.get_streams(first=100)
-
-#print(stream_info)
-
+'''
+randList = []
+while(len(randList) != 5):
+    randNum = random.randint(1,500)
+    if randNum not in randList:
+        randList.append(randNum)
+print(randList)
+'''
+i = 0
+view_count = []
+data = []
+cursor = ''
+for var in range(226):
+    stream_info = twitch.get_streams(first=100,after=cursor)
+    cursor = stream_info['pagination']['cursor']
+    if (var == i * 25):
+        print("Update number:",i+1)
+        data += stream_info['data']
+        uid = []
+        try:
+            for l in range(100):
+                uid.append(stream_info['data'][l]['user_login'])
+        except IndexError:
+            print("Error occured, Index Number:",l)
+        temp = twitch.get_users(logins = uid)
+        view_count += temp['data']
+        l = 0
+        i += 1
+        
+    
+        
 filename = 'twitch_data.csv'
 headers = ['id', 'user_id', 'user_login', 'user_name', 'game_id', 'game_name', 'type', 'title', 'viewer_count', 'started_at', 'language', 'thumbnail_url', 'tag_ids', 'is_mature', 'follow_count','total_views']
 
-data = stream_info['data']
 
 fileEmpty = os.stat(filename).st_size == 0
 
-with open(filename, 'a', newline='') as f:
+
+with open(filename, 'a', newline = '', encoding="utf-8") as f:
     d = DictWriter(f, fieldnames=headers)
     if fileEmpty:
         d.writeheader()
     #print(type(data))
     for row in data:
-        #print(row['user_id'])
+        for count,num in enumerate(view_count):
+            if row['user_id'] == view_count[count]['id']:
+                row['total_views'] = view_count[count]['view_count']
+                break;
         follow_count = twitch.get_users_follows(to_id=row['user_id'])
-        view_count = twitch.get_users(logins=row['user_login'])
-        row['total_views'] = view_count['data'][0]['view_count']
         row['follow_count'] = follow_count['total']
     print('done')
     d.writerows(data)
-    f.close()
+    f.close
