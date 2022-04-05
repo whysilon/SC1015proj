9ee909dde2497fb151ea2419caaa6e726fe3094f
Han <77002201+harpervin@users.noreply.github.com>
2022-04-06 01:14:57 +0800

    editted script to add to test_data2 and 12am 06/04

--- new_scraper.py
+++ new_scraper.py
@@ -50,7 +50,7 @@ for var in range(100):
     i += 1
 
         
-filename = 'test_data.csv'
+filename = 'test_data2.csv'
 headers = ['id', 'user_id', 'user_login', 'user_name', 'game_id', 'game_name', 'type', 'title', 'viewer_count', 'started_at', 'language', 'thumbnail_url', 'tag_ids', 'is_mature', 'follow_count','total_views']
 
 
